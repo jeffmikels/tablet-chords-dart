06e12fd9fea4e25245b716e95e3f60fb6f21c2ad
LCC Web User <lcc_web@peter.lafayettecc.org>
2022-12-16 16:27:37 -0500

    adding build step and service file

--- start.sh
+++ start.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-dart bin/server.dart >> debug.log 2>&1
+#/usr/bin/dart bin/server.dart >> debug.log 2>&1
+
+if [ -f bin/server ]
+then
+	echo "Running pre-built binary... if this is out of date, delete bin/server or manually run build.sh"
+	./bin/server
+else
+	echo "Server binary not found, running build first"
+	./build.sh
+	
+	if [ -f bin/server ]
+	then
+		./bin/server
+	else
+		echo "Server build failed... attempting to run from dart file"
+		/usr/bin/dart bin/server.dart
+	fi
+fi
